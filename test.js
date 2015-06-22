388583ef69e8b1b9ac06fb8c50edfa77c7a4f32f
Joe <bazzooka@gmail.com>
2015-06-22 16:25:41 +0200

    add test for travis

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
 'use strict';
 
-var pluriel = require('./');
+var test = require('ava'),
+    pluriel = require('./');
+
 
 test(function (t) {
-    t.assert(pluriel('test', 0) === 'test');
+    //t.assert(pluriel('test', 0) === 'test');
+    t.assert(pluriel('test', 5) === 'tests');
+    t.assert(pluriel('chou', 5) === 'choux');
+    t.assert(pluriel('poireau', 5) === 'poireaux');
+    t.assert(pluriel('ananas', 5) === 'ananas');
+    t.assert(pluriel('hero', 'heroes', 5) === 'heroes');
     t.end();
 });
